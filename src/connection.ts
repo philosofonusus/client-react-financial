4afd46dfc89c4343cd5ab626eb7acd85c608646c
tentacle <my.tentacles.are.in.you@gmail.com>
2021-10-09 07:37:00 +0300

    s

--- src/connection.ts
+++ src/connection.ts
@@ -3,7 +3,7 @@ import socketSubscribe from "./utils/socketSubscribe";
 const socket = new WebSocket('wss://ws.iqoption.com/echo/websocket');
 
 socket.onopen = _ => {
-    socket.send("{\"name\":\"authenticate\",\"request_id\":\"1631649945_706275013\",\"local_time\":29017,\"msg\":{\"ssid\":\"e25c41aa727fd4c27eff9eb87e0dfdb6\",\"protocol\":3,\"session_id\":\"\",\"client_session_id\":\"\"}}");
+    socket.send("{\"name\":\"authenticate\",\"request_id\":\"1631649945_706275013\",\"local_time\":29017,\"msg\":{\"ssid\":\"652f8978ea311db6c2a1263b1e5f19cc\",\"protocol\":3,\"session_id\":\"\",\"client_session_id\":\"\"}}");
 };
 socket.onmessage = event => socketSubscribe(JSON.parse(event.data))
 
